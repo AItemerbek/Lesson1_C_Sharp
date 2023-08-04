5d2a272d9a8febf56abc94d6c06e063ab298dbc5
alekseytemerbek <tech.temerbek@gmail.com>
2023-08-04 15:42:51 +0300

    add Lesson_009_MethodIntro

---
+++ Lesson_009_Method_Intro/Program.cs
@@ -0,0 +1,56 @@
+﻿int MaximumNumber(int argument1, int argument2, int argument3)
+{
+    int result = argument1;
+    if (argument2 > result) result = argument2;
+    if (argument3 > result) result = argument3;
+    return result;
+}
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int FindIndx(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = FindIndx(array, 444);
+Console.WriteLine(pos);
